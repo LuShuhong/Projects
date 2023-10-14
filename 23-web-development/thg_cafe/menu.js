fa7cb1b7299d7e54d173d2ce5df2f30b83feb8c5
Shuhong Lu <uklawson@gmail.com>
2023-10-14 17:25:10 +0100

    menu setup

---
+++ 23-web-development/thg_cafe/menu.js
@@ -0,0 +1,33 @@
+const menu = `
+id,item,cost,category,allergens
+1,Orange Juice,2,drink,fruit
+2,Apple Juice,2,drink,fruit
+3,Coke,3,drink,
+4,Pepsi,0.1,drink,
+5,Flat White,3.5,drink,dairy
+6,Latte,3.5,drink,dairy
+7,Americano,3,drink,
+8,Tea,2,drink,dairy
+9,Bacon Sandwich,5,main,
+10,Sausage Sandwich,5,main,
+11,Southern Fried Chicken Wrap,6,main,dairy
+12,Tomato Panini,7,main,
+13,Pesto Salad,4,main,
+14,Ribeye Steak,25,main,
+15,Prawn Linguini,20,main,
+16,Beer,5,drink,
+17,Vodka,6,drink,
+18,Cider,6,drink,
+19,Champagne,10,drink,
+20,Gingerbread Man,1,dessert,
+21,Chocolate Brownie,3,dessert,
+22,Tiramisu,10,dessert,dairy
+23,New York Cheesecake,7,dessert,dairy
+24,Chips,3.5,side,
+25,Onion Rings,4,side,
+26,Potato Skins,7,side,
+27,Rice,1,side,
+28,Salad,6,side,
+`;
+
+
