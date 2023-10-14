29fc055108daabfd3371ab6850a7af4b3da0f2ac
Shuhong Lu <uklawson@gmail.com>
2023-10-14 12:56:41 +0100

    solve most common word and number of unique words

--- 23-intro-programming/assignments/shakespeare/code.py
+++ 23-intro-programming/assignments/shakespeare/code.py
@@ -5,9 +5,10 @@ type(text)
 len(text)
 text[1:20]
 
+word_dict={}
 #longest word
 
-def find_longest_word(text):
+def longest_word(text):
     longest_word = text[0]
     longest_word_length = len(longest_word)
     for i in range(0,len(text)):
@@ -19,11 +20,23 @@ def find_longest_word(text):
 
 #longest palidrome
 
-def find_longest_palidrome():
+def longest_palidrome():
     palidrome_list = []
     for i in text:
         if i == i[::-1]:
             palidrome_list.append(i)
 
-    return find_longest_word(palidrome_list)
+    return longest_word(palidrome_list)
 
+def most_common_word():
+    for i in text:
+        if not i in word_dict:
+            word_dict[i] = 1
+        word_dict[i] += 1
+
+
+    return max(word_dict,key = lambda k: word_dict[k])
+
+def uniq_words():
+    most_common_word()
+    return len(word_dict)
