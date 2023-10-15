69ee17bb98ea4626fd0f28ac1a48d39ed4bcafce
Shuhong Lu <uklawson@gmail.com>
2023-10-15 12:22:15 +0100

    define classes and print func

--- 23-intro-programming/assignments/09_exercise.py
+++ 23-intro-programming/assignments/09_exercise.py
@@ -8,3 +8,58 @@
 # Consider representing triangles with 3 side lengths
 #  and using Heron's formula to calculate the area.
 # - https://en.wikipedia.org/wiki/Heron%27s_formula
+
+
+from math import pi
+
+class Shape:
+    def __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        return f"I am a {self.name}"
+
+
+
+class Circle(Shape):
+    def __init__(self,radius):
+        super().__init__("circle")
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius * self.radius
+
+
+class Triangle(Shape):
+    def __init__(self,s1,s2,s3):
+        super().__init__("triangle")
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+
+    def area(self):
+        s = 0.5 * (self.s1 + self.s2 + self.s3)
+        area = (s * (s-self.s1) * (s -self.s2) * (s-self.s3) )** 0.5
+        return area
+
+
+class Square(Shape):
+    def __init__(self,length):    
+        super().__init__("square")
+        self.length = length
+
+    def area(self):
+        return self.length ** 2
+
+
+if __name__ == "__main__":
+    circle1 = Circle(3)
+    circle2 = Circle(4)
+    triangle1 = Triangle(2,3,4)
+    triangle2 = Triangle(3,4,5)
+    triangle3 = Triangle(4,5,6)
+    square1 = Square(10)
+
+    print (circle1)
+    print(triangle2)
+    print(square1)
