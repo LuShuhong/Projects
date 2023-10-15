655dbe782b89506f23a25e0b2e5a69428fd9b7b9
Shuhong Lu <uklawson@gmail.com>
2023-10-15 11:08:36 +0100

    setup OOP exercise

---
+++ 23-intro-programming/assignments/09_exercise.py
@@ -0,0 +1,10 @@
+# Create 2 circles, 3 triangles and a square of different
+#  sizes
+# Add the shapes to a list
+# 1. Find the total area of all the shapes
+# 2. If we wanted to give each shape an x,y coordinate to
+    #  position them in space how could we change the classes to achieve that
+
+# Consider representing triangles with 3 side lengths
+#  and using Heron's formula to calculate the area.
+# - https://en.wikipedia.org/wiki/Heron%27s_formula
