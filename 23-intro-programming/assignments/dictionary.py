69db4fc8d495feaefe284d862fb693494047675d
Shuhong Lu <uklawson@gmail.com>
2023-11-25 16:31:25 +0000

    implement delete and resizing

--- 23-intro-programming/assignments/dictionary.py
+++ 23-intro-programming/assignments/dictionary.py
@@ -6,10 +6,12 @@ class Node:
         return f'{self.key}:{self.value}'
 
 class Dictionary:
-    def __init__(self):
+    def __init__(self,capacity = 2,load_factor = 2):
         self.length = 0
         self.dict = []
-        for i in range(5):
+        self.capacity = capacity
+        self.load_factor = load_factor
+        for i in range(capacity):
             self.dict.append([])
 
     def get_hashed_index(self,key):
@@ -24,24 +26,49 @@ class Dictionary:
                 i.value = value
                 break
         else:
+            if self.length/self.capacity>self.load_factor:
+                self._resize()
             self.dict[hashed_index].append(Node(key,value))
+            self.length += 1
 
 
 
     def lookup(self,key):
         hashed_index = self.get_hashed_index(key)
         if not self.dict[hashed_index]:
-            raise Invalid_key("Invalid Dictionary Key")
+            raise InvalidKey("Invalid Dictionary Key")
         else:
             for i in self.dict[hashed_index]:
                 if i.key == key:
                     return i.value
-            raise Invalid_key("Invalid Dictionary Key")
-    
+            raise InvalidKey("Invalid Dictionary Key")
+        
+    def delete(self, key):
+        hashed_index = self.get_hashed_index(key)
+        if not self.dict[hashed_index]:
+            raise InvalidKey("Invalid Dictionary Key")
+        else:
+            for i,node in enumerate(self.dict[hashed_index]):
+                if node.key == key:
+                    del self.dict[hashed_index][i]
+                    self.length -= 1
+                    return
+            raise InvalidKey("Invalid Dictionary Key")
+        
+
+    def _resize(self):
+        self.capacity = self.capacity *2
+        new_dict = [[] for i in range(self.capacity)]
+        for i in self.dict:
+            for j in i:
+                hashed_index = hash(j.key)%(self.capacity)
+                new_dict[hashed_index].append(Node(j.key,j.value))
+        self.dict = new_dict
+
     def __repr__(self) -> str:
         return str(self.dict)
 
-class Invalid_key(Exception):
+class InvalidKey(Exception):
     pass
     
 d = Dictionary()
@@ -55,6 +82,5 @@ d.insert('Alibaba',400)
 d.insert('Arms',50)
 d.insert('THG',1)
 print(d)
-
-print(d.lookup('THG'))
-print(d.lookup('Amazon'))
+# print(d.lookup('THG'))
+# print(d.lookup('Amazon'))
