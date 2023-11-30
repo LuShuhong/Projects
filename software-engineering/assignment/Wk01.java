8651e67fa65fa46d07b7ac02416ed2192ab233ed
Shuhong Lu <uklawson@gmail.com>
2023-11-30 13:34:26 +0000

    hourglass complete

---
+++ software-engineering/assignment/Wk01.java
@@ -0,0 +1,28 @@
+public class Wk01 {
+    public static void main(String[] args){
+        int length = Integer.parseInt(args[0]);
+        String line = "";
+        for(int i = 0; i < 2*length-1; i++){
+                for (int j = 0; j<2*length -1; j++){
+                if(i<length){
+                    if(j<i || j>=2*length-1-i){
+                        line += " ";
+                    }else{
+                        line += "*";
+                    }
+                }
+                else{
+                    if(j+i<2*length-2||j>i){
+                        line += " ";
+                    }else{
+                        line += "*";
+                    }
+                }
+
+           
+            }
+             System.out.println(line);
+            line = "";
+        }
+    }
+}
