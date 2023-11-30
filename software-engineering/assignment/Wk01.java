e82971ee8a807ac468df96841b96d9b4c8bc908c
Shuhong Lu <uklawson@gmail.com>
2023-11-30 16:39:40 +0000

    geralt complete

--- software-engineering/assignment/Wk01.java
+++ software-engineering/assignment/Wk01.java
@@ -24,5 +24,29 @@ public class Wk01 {
              System.out.println(line);
             line = "";
         }
+
+        Wk01.witcherFinder(args[1]);
     }
+
+    public static boolean witcherFinder(String str){
+        String lowerCaseStr = str.toLowerCase();
+        int num = 0;
+        if(lowerCaseStr.contains("!geralt!")){
+            for(int i = 0; i<lowerCaseStr.length();i++){
+                if(Character.isDigit(lowerCaseStr.charAt(i))){
+                    int numValue = Character.getNumericValue(lowerCaseStr.charAt(i));
+                    num += numValue;
+                }
+            }
+            System.out.println(num);
+            if(num == 10){
+                System.out.println("very true");
+                return true;
+            }
+
+        }
+        System.out.println("very wrong");
+        return false;
+    }
+
 }
